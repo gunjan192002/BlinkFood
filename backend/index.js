02ac58c53f90b2f70f4ed3cc5850cd9d0581b3d4
Shivang06 <94442113+Shivang06@users.noreply.github.com>
2023-07-14 19:59:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 // const port = 5000;
 app.use(cors({origin: true, credentials: true}));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://gofoodfrontend-7heg.onrender.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept"

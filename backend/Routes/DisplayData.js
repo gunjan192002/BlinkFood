902ec6dabb97dcd108e94755bf22680bca434b19
Shivang Gupta <94442113+Shivang06@users.noreply.github.com>
2024-03-11 19:38:21 +0530

    Update DisplayData.js

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -1,14 +1,15 @@
+const { foodItems, categoryItems } = require('../db')
 const express = require('express');
 const router = express.Router()
 
 router.get("/foodData", async (req,res)=>{
     try{
         // console.log(global.food_items);
-       return res.send([global.food_items,global.category_items]);
+        return res.send([foodItems,categoryItems]);
     }
     catch(error){
         console.error(error.message);
-      return res.send("server Error");
+       return res.send("server Error");
     }
 })
 

0ab069da1c8aeb6350d974e5763ce47573a82137
Gunjan Agrawal <99130166+gunjan192002@users.noreply.github.com>
2024-11-06 20:05:01 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-// const mongoURI = 'mongodb+srv://shivang:shivang@cluster0.51eelfu.mongodb.net/gofoods?retryWrites=true&w=majority';
-const mongoURI = 'mongodb://shivang:shivang@ac-chwfz0i-shard-00-00.51eelfu.mongodb.net:27017,ac-chwfz0i-shard-00-01.51eelfu.mongodb.net:27017,ac-chwfz0i-shard-00-02.51eelfu.mongodb.net:27017/gofoods?ssl=true&replicaSet=atlas-12gmst-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoURI = 'mongodb://Gunjan:Gunjan@ac-chwfz0i-shard-00-00.51eelfu.mongodb.net:27017,ac-chwfz0i-shard-00-01.51eelfu.mongodb.net:27017,ac-chwfz0i-shard-00-02.51eelfu.mongodb.net:27017/gofoods?ssl=true&replicaSet=atlas-12gmst-shard-0&authSource=admin&retryWrites=true&w=majority'
 const url = 'food_items';
 console.log(url);
 let foodItems = [];
